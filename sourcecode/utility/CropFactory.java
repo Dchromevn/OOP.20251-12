2cee5ccd47af64990d851d9ec15b6794007a14d5
vdmhth <thao.vdm2416752@sis.hust.edu.vn>
2026-01-12 21:50:16 +0700

    refactor crop

--- sourcecode/utility/CropFactory.java
+++ sourcecode/utility/CropFactory.java
@@ -1,18 +1,12 @@
 package utility;
 
-import model.crops.*;
+import model.crops.Crop;
 
 public class CropFactory {
     private static int cropIdCounter = 0;
     public static Crop createCrop(CropType type, Point position) {
         String id = type.getCropName() + "_" + (++cropIdCounter);
-        return switch (type) {
-            case WHEAT -> new Wheat(id, position);
-            case CORN -> new Corn(id, position);
-            case TOMATO -> new Tomato(id, position);
-            case CARROT -> new Carrot(id, position);
-            case PUMPKIN -> new Pumpkin(id, position);
-        };
+        return new Crop(id, position, type);
     }
 }
 
